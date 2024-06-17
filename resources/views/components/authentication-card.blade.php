fc664f276ad8b9e17d64d2923a4fc5bec9a38912
Mukul Tech <mukulsingh97087@gmail.com>
2024-06-17 22:28:39 +0530

    gyhh

---
+++ resources/views/components/authentication-card.blade.php
@@ -0,0 +1,9 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <div>
+        <img src="https://mbatmbu.com/assets/upload/company_profile/logo/logo_1.jpg" class="img-responsive" style="box-shadow: 4px 0px 8px 3px #fff;max-width: 110%;">
+    </div>
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
