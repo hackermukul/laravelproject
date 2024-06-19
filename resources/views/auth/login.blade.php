4d45d43666bbc75e3bf11e7e5d46885a70cb65e9
Mukul Tech <mukulsingh97087@gmail.com>
2024-06-19 23:41:02 +0530

    test5

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
+ 
 
                 <x-button class="ms-4">
                     {{ __('Log in') }}
