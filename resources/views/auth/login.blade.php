7ac91244622a351e8800c67192c187ef1fa9054a
Mukul Tech <mukulsingh97087@gmail.com>
2024-06-30 22:47:33 +0530

    n

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
-            
+            <h1>tesrt</h1>
         <x-validation-errors class="mb-4" />
 
         @session('status')
@@ -11,7 +11,7 @@
                 {{ $value }}
             </div>
         @endsession
-        
+         
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <div>
